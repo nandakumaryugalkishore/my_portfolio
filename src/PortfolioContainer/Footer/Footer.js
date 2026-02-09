d3a0beaf4d0222f16ede6caa656b600e4ce69e7b
nandakumaryugalkishore <nandakumaryugalkishore@gmail.com>
2026-02-09 02:04:44 -0500

    Second commit

---
+++ src/PortfolioContainer/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div>
+        <div className='footer-container'>
+            <div className='footer-parent'>
+                <img src={require ('../../assets/Home/shape-bg.png')} alt='No Asset'/>
+            </div>
+        </div>
+    </div>
+  )
+}
