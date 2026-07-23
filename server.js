9ee532ada77d35dc5416cd8acc297f4db1b7cde4
nandakumaryugalkishore <nandakumaryugalkishore@gmail.com>
2026-07-23 15:43:44 -0400

    Fix duplicate cors import

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const path = require("path")
 
 const contactRoute = require('./route/contactRoute')
+app.use("/api", contactRoute);
 
 const app = express();
 app.use(express.json());
